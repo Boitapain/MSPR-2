cc79b5c4b1c80b340f9072d3b463af9d2833b18a
hugo <hugo.drevet@hotmail.fr>
2025-07-03 13:53:09 +0200

    03/07/2025 : refactor manage user

--- app/frontend/dashboard_pages/manage_users.py
+++ app/frontend/dashboard_pages/manage_users.py
@@ -4,6 +4,65 @@ import requests
 import time
 from translations import load_translations
 
+def fetch_users(api_url):
+    response = requests.get(f"{api_url}/users")
+    response.raise_for_status()
+    data = response.json().get("users")
+    return pd.DataFrame(data, columns=["id", "name", "email", "country", "isAdmin"])
+
+def update_users(api_url, users_edited):
+    data_json = users_edited.to_json(orient="records")
+    response = requests.put(
+        f"{api_url}/update_users",
+        json={"users": data_json},
+        headers={"Content-Type": "application/json"}
+    )
+    return response
+
+def show_progress_bar(t):
+    my_bar = st.progress(0, text=t['update_progress'].format(percent=0))
+    my_bar.help(t['help_messages']['progress_bar'])
+    for percent_complete in range(100):
+        time.sleep(0.01)
+        my_bar.progress(
+            percent_complete + 1,
+            text=t['update_progress'].format(percent=percent_complete + 1)
+        )
+    time.sleep(1)
+    my_bar.empty()
+
+def show_admin_editor(users, t, api_url):
+    cols = st.columns([1, 4, 1])
+    with cols[1]:
+        users_edited = st.data_editor(
+            users,
+            column_config={
+                "id": st.column_config.NumberColumn(),
+                "name": st.column_config.TextColumn(),
+                "email": st.column_config.TextColumn(),
+                "country": st.column_config.TextColumn(default="USA"),
+                "isAdmin": st.column_config.CheckboxColumn(default=False)
+            },
+            num_rows="dynamic"
+        )
+        if st.button(t['update_button'], help=t['help_messages']['update_button']):
+            users_edited["country"] = users_edited["country"].fillna("USA")
+            if users_edited.isnull().values.any():
+                st.error(t['empty_fields_error'])
+            else:
+                response = update_users(api_url, users_edited)
+                if response.status_code == 200:
+                    show_progress_bar(t)
+                    st.session_state["users_updated"] = True
+                    st.rerun()
+                else:
+                    st.error(t['update_error'])
+
+def show_readonly_view(users, t):
+    cols = st.columns([1, 2, 1])
+    with cols[1]:
+        st.dataframe(users, help=t['help_messages']['readonly_view'])
+
 def manage_users(user):
     lang = st.session_state.get('language', 'en')
     translations = load_translations(lang)
@@ -16,64 +75,10 @@ def manage_users(user):
         st.rerun()
 
     try:
-        response = requests.get(f"{st.session_state['API_URL']}/users")
-        response.raise_for_status()
-        data = response.json().get("users")
-        users = pd.DataFrame(data, columns=[
-            "id", "name", "email", "country", "isAdmin"
-        ])
-
+        users = fetch_users(st.session_state['API_URL'])
         if user["isAdmin"]:
-            cols = st.columns([1, 4, 1])
-            with cols[1]:
-                users_edited = st.data_editor(
-                    users,
-                    column_config={
-                        "id": st.column_config.NumberColumn(),
-                        "name": st.column_config.TextColumn(),
-                        "email": st.column_config.TextColumn(),
-                        "country": st.column_config.TextColumn(
-                            default="USA"
-                        ),
-                        "isAdmin": st.column_config.CheckboxColumn(
-                            default=False
-                        )
-                    },
-                    num_rows="dynamic"
-                )
-                if st.button(
-                    t['update_button'],
-                    help=t['help_messages']['update_button']
-                ):
-                    users_edited["country"] = users_edited["country"].fillna("USA")
-                    if users_edited.isnull().values.any():
-                        st.error(t['empty_fields_error'])
-                    else:
-                        data_json = users_edited.to_json(orient="records")
-                        response = requests.put(
-                            f"{st.session_state['API_URL']}/update_users",
-                            json={"users": data_json},
-                            headers={"Content-Type": "application/json"}
-                        )
-                        if response.status_code == 200:
-                            my_bar = st.progress(0, text=t['update_progress'].format(percent=0))
-                            my_bar.help(t['help_messages']['progress_bar'])
-
-                            for percent_complete in range(100):
-                                time.sleep(0.01)
-                                my_bar.progress(
-                                    percent_complete + 1,
-                                    text=t['update_progress'].format(percent=percent_complete + 1)
-                                )
-                            time.sleep(1)
-                            my_bar.empty()
-                            st.session_state["users_updated"] = True
-                            st.rerun()
-                        else:
-                            st.error(t['update_error'])
+            show_admin_editor(users, t, st.session_state['API_URL'])
         else:
-            cols = st.columns([1, 2, 1])
-            with cols[1]:
-                st.dataframe(users, help=t['help_messages']['readonly_view'])
-    except requests.exceptions.RequestException as e:
+            show_readonly_view(users, t)
+    except requests.exceptions.RequestException:
         st.error(t['no_data_error'])
